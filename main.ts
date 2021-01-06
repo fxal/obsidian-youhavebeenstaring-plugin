51b2f0e2f6c2645d77ef61f6c32ab74f8e31259d
Felix <gitlab@fluid.myddns.me>
2021-01-06 07:26:44 +0100

    1.0.2

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
 import { App, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 export default class YouHaveBeenStaring extends Plugin {
-	initialLoadTimestamp: number
-	statusBarItem: HTMLElement
+	initialLoadTimestamp: number;
+	statusBarItem: HTMLElement;
 
 	async onload() {
-		this.initialLoadTimestamp = Date.now()
-		this.statusBarItem = this.addStatusBarItem()
-		this.registerInterval(window.setInterval(() => this.showTimeSinceLoad(), 1000))
+		this.initialLoadTimestamp = Date.now();
+		this.statusBarItem = this.addStatusBarItem();
+		this.registerInterval(window.setInterval(() => this.showTimeSinceLoad(), 1000));
 	}
 
 	showTimeSinceLoad(): void {
 		if(this.initialLoadTimestamp) {
-			let moment = (window as any).moment
-			let fromNow = moment(this.initialLoadTimestamp).fromNow(true)
-			this.statusBarItem.setText(`You have been staring at your vault for ${fromNow}`)
+			let moment = (window as any).moment;
+			let fromNow = moment(this.initialLoadTimestamp).fromNow(true);
+			this.statusBarItem.setText(`You have been staring at your vault for ${fromNow}`);
 		}
 	}
 
